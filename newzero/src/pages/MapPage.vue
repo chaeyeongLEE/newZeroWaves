e929a0b05ba9c7887ce626ab3167ddd0246a1639
chaeyeongLEE <dh503401@naver.com>
2024-07-15 14:31:46 +0900

    지도검색 UI 개발완료

--- newzero/src/pages/MapPage.vue
+++ newzero/src/pages/MapPage.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <h1>kakaoMap</h1>
-    <div id="map"></div>
+  <div :class="$style.container">
+    <form :class="$style.searchBox" @submit="searchPlace()">
+      <input type="text" v-model="searchData" placeholder="장소명을 검색해주세요." />
+      <button type="submit">검색</button>
+    </form>
+    <div id="map" :class="$style.map"></div>
   </div>
 </template>
 
@@ -10,7 +13,8 @@ export default {
   name: "MapPage",
   data() {
     return {
-      map: null
+      map: null,
+      searchData: '',
     };
   },
   mounted() {
@@ -23,8 +27,7 @@ export default {
   methods: {
     loadScript(){
       const script = document.createElement("script");
-      // 6fff259c639722b1a997ad8472e9a88a
-      script.src ="//dapi.kakao.com/v2/maps/sdk.js?appkey=6fff259c639722b1a997ad8472e9a88a&autoload=false"
+      script.src ="//dapi.kakao.com/v2/maps/sdk.js?appkey=1a9bd286f80311a96542bac4808401e1&autoload=false";
       script.onload = () => window.kakao.maps.load(this.loadMap);
       document.head.appendChild(script);
     },
@@ -38,17 +41,47 @@ export default {
       this.loadMaker();
     },
     loadMaker() {
-      const markerPosition = new window.kakao.maps.LatLng(33.450701, 126.570667)
-      const marker = new window.kakao.maps.Marker({position:markerPosition})
+      const markerPosition = new window.kakao.maps.LatLng(33.450701, 126.570667);
+      const marker = new window.kakao.maps.Marker({ position:markerPosition });
       marker.setMap(this.map);
+    },
+    searchPlace() {
     }
   }
 }
 </script>
 
-<style scoped>
-#map {
+<style module>
+.container {
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+}
+.searchBox{
+  width: 300px;
+  background-color: #e9edc9;
+  padding: 1rem;
+  border-radius: 0.3rem;
+}
+.searchBox > input {
+  border: 0;
+  outline: none;
+  border-radius: 0.3rem;
+  width: 170px;
+  padding: 0.3rem;
+}
+.searchBox > button {
+  border-radius: 0.3rem;
+  border: 1px solid #d4a373;
+  background-color: #d4a373;
+  padding: 0.2rem;
+  width: 50px;
+  margin-left: 10px;
+  color: #ffffff;
+}
+.map {
   width: 100%;
   height: 700px;
+  border-radius: 0.3rem;
 }
 </style>
