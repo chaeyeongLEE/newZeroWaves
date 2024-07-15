550047c2c13c27a1e7bf302d0bd429a69ac0b41e
chaeyeongLEE <dh503401@naver.com>
2024-07-15 16:07:52 +0900

    지도 검색 시 마커 생성

--- newzero/src/pages/MapPage.vue
+++ newzero/src/pages/MapPage.vue
@@ -1,8 +1,8 @@
 <template>
   <div :class="$style.container">
-    <form :class="$style.searchBox" @submit="searchPlace()">
-      <input type="text" v-model="searchData" placeholder="장소명을 검색해주세요." />
-      <button type="submit">검색</button>
+    <form :class="$style.searchBox" @submit.prevent="searchPlace">
+      <input type="text" v-model="searchData" placeholder="장소명을 검색해주세요." id="keyword"/>
+      <button type="submit" id="submitBtn" @click.prevent="searchPlace">검색</button>
     </form>
     <div id="map" :class="$style.map"></div>
   </div>
@@ -15,40 +15,64 @@ export default {
     return {
       map: null,
       searchData: '',
+      ps: null,
     };
   },
   mounted() {
-    if(window.kakao && window.kakao.maps) {
+    if (window.kakao && window.kakao.maps) {
+      this.ps = new window.kakao.maps.services.Places();
       this.loadMap();
     } else {
       this.loadScript();
     }
   },
   methods: {
-    loadScript(){
+    loadScript() {
       const script = document.createElement("script");
-      script.src ="//dapi.kakao.com/v2/maps/sdk.js?appkey=1a9bd286f80311a96542bac4808401e1&autoload=false";
       script.onload = () => window.kakao.maps.load(this.loadMap);
+      script.src = "//dapi.kakao.com/v2/maps/sdk.js?appkey=1a9bd286f80311a96542bac4808401e1&autoload=false&libraries=services";
       document.head.appendChild(script);
     },
-    loadMap(){
-      const container = document.getElementById("map"); //지도를 담을 DOM 영역
+    loadMap() {
+      const container = document.getElementById("map");
       const options = {
         center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3,
+        level: 1,
       };
       this.map = new window.kakao.maps.Map(container, options);
-      this.loadMaker();
-    },
-    loadMaker() {
-      const markerPosition = new window.kakao.maps.LatLng(33.450701, 126.570667);
-      const marker = new window.kakao.maps.Marker({ position:markerPosition });
-      marker.setMap(this.map);
     },
     searchPlace() {
+      const keyword = this.searchData.trim();
+      if (!keyword) {
+        alert('키워드를 입력해주세요!');
+        return;
+      }
+      this.ps.keywordSearch(keyword, this.placesSearchCB);
+    },
+    placesSearchCB(data, status) {
+      if (status === window.kakao.maps.services.Status.OK) {
+        this.displayPlaces(data);
+      } else if (status === window.kakao.maps.services.Status.ZERO_RESULT) {
+        alert("검색 결과가 존재하지 않습니다.");
+      } else if (status === window.kakao.maps.services.Status.ERROR) {
+        alert("검색 결과 중 오류가 발생했습니다.");
+      }
+    },
+    displayPlaces(places) {
+      const bounds = new window.kakao.maps.LatLngBounds();
+      for (let i = 0; i < places.length; i++) {
+        this.displayMarker(places[i]);
+        bounds.extend(new window.kakao.maps.LatLng(places[i].y, places[i].x));
+      }
+      this.map.setBounds(bounds);
+    },
+    displayMarker(place) {
+      const markerPosition = new window.kakao.maps.LatLng(place.y, place.x);
+      const marker = new window.kakao.maps.Marker({ position: markerPosition });
+      marker.setMap(this.map);
     }
   }
-}
+};
 </script>
 
 <style module>
@@ -57,12 +81,14 @@ export default {
   flex-direction: row;
   gap: 1rem;
 }
-.searchBox{
+
+.searchBox {
   width: 300px;
   background-color: #e9edc9;
   padding: 1rem;
   border-radius: 0.3rem;
 }
+
 .searchBox > input {
   border: 0;
   outline: none;
@@ -70,6 +96,7 @@ export default {
   width: 170px;
   padding: 0.3rem;
 }
+
 .searchBox > button {
   border-radius: 0.3rem;
   border: 1px solid #d4a373;
@@ -79,6 +106,7 @@ export default {
   margin-left: 10px;
   color: #ffffff;
 }
+
 .map {
   width: 100%;
   height: 700px;
